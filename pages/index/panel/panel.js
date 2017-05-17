02fdd951c9b785ce97fd5261d30bad36afeebfc5
unknown <xxdalston@gmail.com>
2017-05-18 00:55:02 +0800

    add half-faked share feature for detail post, add full title for detail post

--- pages/index/panel/panel.js
+++ pages/index/panel/panel.js
@@ -1,8 +1,11 @@
 // pages/index/panel/panel.js
 
+var app = getApp()
+
 Page({
     data: {
-        list: []
+        list: [],
+        title: 'Growth',      
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
@@ -11,6 +14,9 @@ Page({
             this.setData({ list: detail });
         } catch (e) {
         }
+
+        // 获取 subTitle，并拼接完整 title 作为分享 title
+        this.setFullTitle(options.name,options.subTitle); 
     },
     onReady: function () {
         // 页面渲染完成
@@ -23,6 +29,30 @@ Page({
     },
     onUnload: function () {
         // 页面关闭
+    },
+
+    // 根据 name 、 subTitle 拼接完整 title，并作为分享 title
+    // TBD 之后把这类公用方法整合到 app.js 中，提高复用
+    setFullTitle: function(name,subTitle) {     
+      this.setData({ title: subTitle + app.utils.getTitle(name) })
+      wx.setNavigationBarTitle({
+        title: this.data.title
+      })
+    },
+
+    // TBD 目前是假的文章页分享
+    // TBD 有待完善，由于无法通过参数进入具体文章，暂时先返回上一级列表
+    onShareAppMessage: function () {
+      return {
+        title: this.data.title,
+        path: '/pages/index/list/list?name=project',
+        success: function (res) {
+          // 分享成功
+        },
+        fail: function (res) {
+          // 分享失败
+        }
+      }
     },
     setClipboardData: function (e) {
         //复制链接
@@ -39,3 +69,4 @@ Page({
         })
     }
 })
+
--- pages/index/timeline/timeline.js
+++ pages/index/timeline/timeline.js
@@ -1,7 +1,8 @@
 // pages/index/article/article.js
 var that = null,
   detail = null,
-  request = require('../../../common/request.js');
+  request = require('../../../common/request.js'),
+  app = getApp();
 Page({
   data: {
     passages: []
@@ -13,6 +14,9 @@ Page({
       this.setData({ detail: transform(detail) });
     } catch (e) {
     }
+
+    // 获取 subTitle，并拼接完整 title 作为分享 title
+    this.setFullTitle(options.name, options.subTitle); 
   },
   onReady: function () {
     // 页面渲染完成
@@ -26,7 +30,29 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  }
+  },
+  // 根据 name 、 subTitle 拼接完整 title，并作为分享 title
+  setFullTitle: function (name, subTitle) {
+    this.setData({ title: subTitle + app.utils.getTitle(name) })
+    wx.setNavigationBarTitle({
+      title: this.data.title
+    })
+  },
+
+  // TBD 目前是假的文章页分享
+  // TBD 有待完善，由于无法通过参数进入具体文章，暂时先返回上一级列表
+  onShareAppMessage: function () {
+    return {
+      title: this.data.title,
+      path: '/pages/index/list/list?name=project',
+      success: function (res) {
+        // 分享成功
+      },
+      fail: function (res) {
+        // 分享失败
+      }
+    }
+  },
 })
 
 function transform(detail) {
